5c0557d9bf190ab3b482647374f8a421f833302e
ddt-miyazaki <ddt.tk.miyazaki@gmail.com>
2022-03-10 18:14:38 +0900

    演習問題5_5まで追加

---
+++ ensyu5_3/src/ensyu5_3/ensyu5_3.java
@@ -0,0 +1,20 @@
+package ensyu5_3;
+
+/*
+	論理型の変数にtrueやfalseを代入して、その値を表示するプログラムを作成せよ
+*/
+public class ensyu5_3 {
+
+	public static void main(String[] args) {
+		// 論理型の変数にtrueを代入
+		boolean t = true;
+		// 論理型の変数にfalseを代入
+		boolean f = false;
+		// trueの値を表示
+		System.out.println(t);
+		// falseの値を表示
+		System.out.println(f);
+
+	}
+
+}
