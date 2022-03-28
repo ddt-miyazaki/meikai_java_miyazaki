9c90d8b89bc70bfef87973b4f0562fc3556203eb
ddt-miyazaki <ddt.tk.miyazaki@gmail.com>
2022-03-28 16:02:46 +0900

    不要な行の削除

--- sql_chap_2/q2_5_4.sql
+++ sql_chap_2/q2_5_4.sql
@@ -2,5 +2,4 @@
 -- ユーザの氏名をユーザテーブル(usr)から取り出せ
 
 SELECT CONCAT(u.l_name, u.f_name) AS 氏名 FROM usr AS u
-INNER JOIN sales AS sl ON s.s_id = sl.s_id
 WHERE NOT EXISTS ( SELECT * FROM rental AS r WHERE u.user_id = r.user_id);
